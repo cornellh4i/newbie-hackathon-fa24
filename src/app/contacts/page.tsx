e5ea6f978ae7e481555d0d85e15f48dd671fea83
Jason Zheng <jasonz4200@gmail.com>
2024-10-04 21:23:13 -0400

    Fix apostrophe

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,111 +1,131 @@
 import Image from "next/image";
 const Contacts = () => {
-    return (
-        
-      <div className="flex flex-col items-center justify-between items-center min-h-screen py-8 shadow-md w-full">
-        {/* Profile Section */}
-        <nav className="flex justify-between items-center p-6 bg-white shadow-md w-full">
-            <h1 className="text-3xl font-bold text-red-600"><u>Martha Pollack's</u> Portfolio</h1>
-            <ul className="flex space-x-6 text-lg">
-                <li className="relative">
-                <a
-                    href="/about"
-                    className="text-red-600 font-bold hover:font-bold before:content-[''] before:absolute before:left-0 before:top-[-10px] before:w-full before:h-1 before:bg-red-600"
-                >
-                    ABOUT
-                </a>
-                </li>
-                <li>
-                <a
-                    href="/achievements"
-                    className="text-red-600 font-normal hover:font-bold"
-                >
-                    ACHIEVEMENTS
-                </a>
-                </li>
-                <li>
-                <a
-                    href="/fun-facts"
-                    className="text-red-600 font-normal hover:font-bold"
-                >
-                    FUN FACTS
-                </a>
-                </li>
-                <li>
-                <a
-                    href="/contact"
-                    className="text-red-600 font-normal hover:font-bold"
-                >
-                    CONTACT
-                </a>
-                </li>
-            </ul>
-        </nav>
-        <div ></div>
-        <div className="flex flex-left items-center mb-7 lg:w-1/2 flex-row ">
-         
-          <div className="float">
-            <h3 className="mt-2 text-xl text-red-600">
-              <strong><u>LINKEDIN</u></strong>{' '}<br></br>
-              <a href="https://www.linkedin.com/school/cornell-university/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className="text-[#fa98a9] underline text-l">
-                Cornell University
-              </a>
-            </h3>
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-between py-8 shadow-md">
+      {/* Profile Section */}
+      <nav className="flex w-full items-center justify-between bg-white p-6 shadow-md">
+        <h1 className="text-3xl font-bold text-red-600">
+          <u>Martha Pollack&#39;s</u> Portfolio
+        </h1>
+        <ul className="flex space-x-6 text-lg">
+          <li className="relative">
+            <a
+              href="/about"
+              className="font-bold text-red-600 before:absolute before:left-0 before:top-[-10px] before:h-1 before:w-full before:bg-red-600 before:content-[''] hover:font-bold"
+            >
+              ABOUT
+            </a>
+          </li>
+          <li>
+            <a
+              href="/achievements"
+              className="font-normal text-red-600 hover:font-bold"
+            >
+              ACHIEVEMENTS
+            </a>
+          </li>
+          <li>
+            <a
+              href="/fun-facts"
+              className="font-normal text-red-600 hover:font-bold"
+            >
+              FUN FACTS
+            </a>
+          </li>
+          <li>
+            <a
+              href="/contact"
+              className="font-normal text-red-600 hover:font-bold"
+            >
+              CONTACT
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div></div>
+      <div className="flex-left mb-7 flex flex-row items-center lg:w-1/2">
+        <div className="float">
+          <h3 className="mt-2 text-xl text-red-600">
+            <strong>
+              <u>LINKEDIN</u>
+            </strong>{" "}
             <br></br>
-            <p className="mt-1 text-xl text-red-600">
-              <strong><u>PHONE</u></strong>{' '}<br></br>
-              <a href="tel:+1234567890" className="text-[#fa98a9] underline text-l">
-                (123) 456-7890
-              </a>
-            
-            </p>
-            <br></br><br></br>
-            <div className="mb-4">
-            <Image src="/img.png" alt="Martha Pollack" width={300} height={300}/>
+            <a
+              href="https://www.linkedin.com/school/cornell-university/posts/?feedView=all"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-l text-[#fa98a9] underline"
+            >
+              Cornell University
+            </a>
+          </h3>
+          <br></br>
+          <p className="mt-1 text-xl text-red-600">
+            <strong>
+              <u>PHONE</u>
+            </strong>{" "}
+            <br></br>
+            <a
+              href="tel:+1234567890"
+              className="text-l text-[#fa98a9] underline"
+            >
+              (123) 456-7890
+            </a>
+          </p>
+          <br></br>
+          <br></br>
+          <div className="mb-4">
+            <Image
+              src="/img.png"
+              alt="Martha Pollack"
+              width={300}
+              height={300}
+            />
           </div>
-          
-          </div>
-          <div className="bg-[#fa98a9] p-8 rounded-lg shadow-lg w-full max-w-xs text-left">
-            <h2 className="text-xl font-semibold mb-4 text-red-600">EMAIL ME</h2>
-            <form>
-            <label className="block text-sm mb-2 text-white">First Name</label>
+        </div>
+        <div className="w-full max-w-xs rounded-lg bg-[#fa98a9] p-8 text-left shadow-lg">
+          <h2 className="mb-4 text-xl font-semibold text-red-600">EMAIL ME</h2>
+          <form>
+            <label className="mb-2 block text-sm text-white">First Name</label>
             <input
-                type="text"
-                placeholder="First Name"
-                className="w-full p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
+              type="text"
+              placeholder="First Name"
+              className="mb-4 w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
             />
-            
-            <label className="block text-sm mb-2 text-white">Last Name</label>
+
+            <label className="mb-2 block text-sm text-white">Last Name</label>
             <input
-                type="text"
-                placeholder="Last Name"
-                className="w-full p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
+              type="text"
+              placeholder="Last Name"
+              className="mb-4 w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
             />
-            
-            <label className="block text-sm mb-2 text-white">Email</label>
+
+            <label className="mb-2 block text-sm text-white">Email</label>
             <input
-                type="email"
-                placeholder="Email"
-                className="w-full p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
+              type="email"
+              placeholder="Email"
+              className="mb-4 w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
             />
-            
-            <label className="block text mb-2 text-white">Message</label>
+
+            <label className="text mb-2 block text-white">Message</label>
             <textarea
-                placeholder="Message"
-                className="w-full p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#fa98a9] h-32"
+              placeholder="Message"
+              className="mb-4 h-32 w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-[#fa98a9]"
             />
-            
-            <button type="submit" className="w-full bg-red-600 text-white py-2 rounded-full hover:bg-[#fa98a9] transition-colors">
-                Send
+
+            <button
+              type="submit"
+              className="w-full rounded-full bg-red-600 py-2 text-white transition-colors hover:bg-[#fa98a9]"
+            >
+              Send
             </button>
-            </form>
+          </form>
         </div>
-        </div>
-        
-        {/* Email Section with mailto: link */}
-        
       </div>
-    );
-  }
+
+      {/* Email Section with mailto: link */}
+    </div>
+  );
+};
 
 export default Contacts;
